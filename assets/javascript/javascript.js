500893ccd1db19357577b7d4f63dfda6a81fd0a7
pfotis <69961508+pfotis@users.noreply.github.com>
2020-12-06 19:35:50 +1100

    set penalty 10sec to wrong answer

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,8 +4,10 @@ var questionElement = document.querySelector("#question");
 var textElement = document.querySelector("#text");
 var listElement = document.getElementById("list");
 var StartBtn = document.getElementById("startButton");
+var resultElemant = document.querySelector("#prevResult");
 
-var min = 75;
+
+localStorage.secondsLeft= 75;
 var countQuestion = 0;
 var questions = [
   {
@@ -43,24 +45,41 @@ var questions = [
 ];
 
 // set timer
-function countDown(secondsLeft) {
+function countDown() {
   var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeElement.textContent = "Time : " + secondsLeft;
+    timeElement.textContent = "Time : " + localStorage.secondsLeft;
+    localStorage.secondsLeft--;
   }, 1000);
 }
 
 // print the Question to browser
-function printQuestion(i){
-  questionElement.textContent = questions[i].title;
-  for (var j=0; j<questions[i].choices.length; j++){
+function printQuestion(count){
+  questionElement.textContent = questions[count].title;
+  for (var j=0; j<questions[count].choices.length; j++){
     var li = document.createElement("li");
     var button = document.createElement("button");
-    button.textContent = questions[i].choices[j];
+    button.textContent = questions[count].choices[j];
     li.setAttribute("data-index", j);
     li.appendChild(button);
     listElement.appendChild(li);
   }
+}
+function printAnswer(result){
+  var printTime = 2;
+  var hr = document.createElement("hr");
+  var div = document.createElement("div");
+  var timerInterval = setInterval(function() {
+    printTime--;
+    console.log(printTime);
+    div.textContent = result;
+    resultElemant.appendChild(hr);
+    resultElemant.appendChild(div);
+    if(printTime == 0) {
+      resultElemant.removeChild(list.childNodes[0]);
+      resultElemant.removeChild(list.childNodes[0]);
+    }
+
+  }, 1000);
 }
 
 // Start Quiz disappear the the title , text and start button and coming uo the first question
@@ -68,35 +87,35 @@ function startQuiz() {
   textElement.innerHTML = "";
   headerElement.innerHTML = "";
   StartBtn.innerHTML = "";
-  printQuestion(countQuestion);  
+  countDown();
+  printQuestion(countQuestion); 
 }
 
 
 
 StartBtn.addEventListener("click", startQuiz);
-countDown(75);
-console.log(questions.length);
 listElement.addEventListener("click", function(event) {
   var element = event.target;
   var result = "";
   if (element.matches("button") === true) {
     var index = element.parentElement.getAttribute("data-index");
     if(questions[countQuestion].choices[index] == questions[countQuestion].answer){
-      result = "Correct";
+      result = "Correct !";
       console.log(result);
     }
     else{
-      result = "Wrong";
+      result = "Wrong !";
+      localStorage.secondsLeft = localStorage.secondsLeft - 10;
       console.log(result);
-      
     }
     for(var j=0; j<questions[countQuestion].choices.length; j++)
       listElement.removeChild(list.childNodes[0]);
     countQuestion++;
-    printQuestion(countQuestion);
-    if((countQuestion+1) == questions.length){
-      console.log("stop");
-
+    if(countQuestion != questions.length){
+      printQuestion(countQuestion);
+    }
+    else{
+      questionElement.textContent = "All done !";
     }
   }
 });
